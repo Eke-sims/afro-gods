a05631d90c093a0137796547d5fe18ff159c7112
Eke-sims <Eketimileyin@gmail.com>
2021-12-29 11:07:44 +0100

    Improved on the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,31 @@
 import React from 'react'
 import './Header.css'
+import appLogo from '../assets/weth.png'
+import searchicon from '../assets/header/search.png'
 
 const Header = () => {
     return (
         <div className='headerContainer'>
-            <div className='logo'>
-    
+            <div className='logoContainer'>
+                <img src={appLogo} className='appLogo'/>
             </div>
             <div className='searchbarContainer'>
-                <input type='text' className='searchbar' placeholder='search for anything'></input>
+                <div className='searchbariconContainer'>
+                    <img className='searchbaricon' src={searchicon}/>
+                </div>
+                <input type='text' className='searchbar' placeholder='Collection, item or user...'></input>
+
             </div>
             <div className='links'>
                 <p>Drops</p>
                 <p>Marketplace</p>
                 <p>Create</p>
             </div>
+            <div className='themeContainer'>hi</div>
+            <div className='btnContainer'>
+                <button>GET IN</button>
+            </div>
+            
         </div>
     )
 }
