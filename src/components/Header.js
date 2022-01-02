82d513a103fce082e2ce7893a25bc8038ac1a781
Eke-sims <Eketimileyin@gmail.com>
2022-01-02 12:18:10 +0100

    Finished working on the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,30 +2,31 @@ import React from 'react'
 import './Header.css'
 import appLogo from '../assets/weth.png'
 import searchicon from '../assets/header/search.png'
+import themeicon from '../assets/header/theme-switch.png'
 
 const Header = () => {
     return (
         <div className='headerContainer'>
             <div className='logoContainer'>
-                <img src={appLogo} className='appLogo'/>
+                <img src={appLogo} className='appLogo' alt=''/>
             </div>
             <div className='searchbarContainer'>
                 <div className='searchbariconContainer'>
-                    <img className='searchbaricon' src={searchicon}/>
+                    <img className='searchbaricon' src={searchicon} alt=''/>
                 </div>
                 <input type='text' className='searchbar' placeholder='Collection, item or user...'></input>
-
             </div>
             <div className='links'>
                 <p>Drops</p>
                 <p>Marketplace</p>
                 <p>Create</p>
             </div>
-            <div className='themeContainer'>hi</div>
-            <div className='btnContainer'>
-                <button>GET IN</button>
+            <div className='themeContainer'>
+                <img className='themeicon' src={themeicon} alt=''/>
             </div>
-            
+            <div className='btnContainer'>
+                <button className='signupbtn'>GET IN</button>
+            </div>
         </div>
     )
 }
