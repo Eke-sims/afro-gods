e1aee9deea0767be0787f66cd174c9bd5825668c
Eke-sims <Eketimileyin@gmail.com>
2022-01-14 11:27:54 +0100

    starting the main(display) window

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from './components/Header';
 import {useEffect, useState} from 'react'
 import axios from 'axios';
 import Punklist from './components/Punklist';
+import Main from './components/Main';
 
 
 function App() {
@@ -27,6 +28,7 @@ function App() {
   return (
     <div className='app'>
       <Header />
+      <Main />
       <Punklist punklistData={punklistData}/>
     </div>
       
---
+++ src/components/Main.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Main = () => {
+    return (
+        <div className='main'>
+            <div className='mainContainer'>
+                <div className='punkContainer'>
+                    <img  className='selectedpunk' alt=''/>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Main
+
